230ab2bcd998c7ddbaf894e0d442708fc12c46bb
olivier blunt <blunt7690@gmail.com>
2018-03-14 12:32:08 +0100

    Changes for new base image

--- 90-laravel.sh
+++ 90-laravel.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-# Edit the nginx configuation
-php -r "
-\$path = '/etc/nginx/servers/default.conf';
-\$cfg = file_get_contents(\$path);
-
-// Add the rewrite rule in the 'location /' block
-\$pos = strrpos(\$cfg, 'location / {') + 12;
-\$cfg = substr(\$cfg, 0, \$pos)
-    .'
-    # Php rewrite
-    try_files \$uri /index.php?\$query_string;
-    '
-    .substr(\$cfg, \$pos);
-
-// Add 404 redirect before last }
-\$pos = strrpos(\$cfg, '}');
-\$cfg = substr(\$cfg, 0, \$pos)
-    .'
-    error_page 404 /index.php;
-    '
-    .substr(\$cfg, \$pos);
-
-file_put_contents(\$path, \$cfg);"
-
 # Install some php extension
 apk add --update php7-tokenizer
 apk add --update php7-xml
@@ -33,3 +9,11 @@ apk add --update php7-ctype
 apk add --update php7-fileinfo
 apk add --update php7-sockets
 apk add --update php7-dom
+
+# Add the rewrite rules
+echo '
+location / {
+	try_files $uri /index.php?$query_string;
+}
+error_page 404 /index.php;
+' > /etc/nginx/locations/90-laravel.conf
